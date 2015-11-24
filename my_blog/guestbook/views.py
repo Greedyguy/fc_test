0067c7b6b8d9af0e8358d4cfabe5ce9aae53171c
Greedyguy <chgw2001@gmail.com>
2015-11-24 11:35:15 +0900

    2주차 복습 코드

---
+++ my_blog/guestbook/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import HttpResponse
+
+from .models import Post
+
+def list_posts(request):
+	last_post = Post.objects.last()
+	output = '마지막 글 본문 : {}'.format(
+		last_post.content
+	)
+
+	return HttpResponse(output)
